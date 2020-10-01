daa853c0bb4c20124bc02582fe87a88d14d43786
juan <jcasasmail@gmail.com>
2020-10-01 09:14:43 -0400

    updated gitignore

--- src/components/key.js
+++
@@ -1,4 +0,0 @@
-export const STRIPE_FRONT_KEY =
-  "pk_test_51HTrdNKvlLDAjAUzb0c9zHwvq0wAcXOTWThrIscZMRVTc9xcfmqcFm4nLUjij9ZUcwgaewsQuNpBWak2KDQo1p4A00mFA5hRXA";
-export const STRIPE_BACK_KEY =
-  "sk_test_51HTrdNKvlLDAjAUzi8u2jGnHqF2SkxKmRG91jjcq70LxTeJcXeAuX9SuSeuzDQeegVeYrdfa9MBByfAELT0ylVyi00zOCmEdWL";
