04ab991de01297c8c239cfadcac3544756c1c43a
juan <jcasasmail@gmail.com>
2020-10-02 18:28:06 -0400

    prior to adding expo date picker

--- src/screens/DrawerScreens/NewOrderScreen.js
+++ src/screens/DrawerScreens/NewOrderScreen.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const NewOrderScreen = (props) => {
+  const [pickUpDate, setPickUpDate] = useState();
+  const [pickUpTime, setPickUpTime] = useState();
+  const [preference, setPreference] = useState();
+  const [preferecenNote, setPreferenceNote] = useState();
+  const [pickUpAddress, setPickUpAddress] = useState();
+
+  useEffect(() => {
+    console.log("NewOrderScreen loaded");
+  }, []);
+
   return (
     <View>
       <Text>NewOrderScreen</Text>
